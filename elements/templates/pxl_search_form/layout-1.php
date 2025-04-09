995402c36d79d39e1aa0d6eb9769b60c6ea5cbb3
hongducdev <contact.hongduc@gmail.com>
2025-04-09 16:21:10 +0700

    style: improve responsiveness and remove unused hover effect
    Update various SCSS and CSS files to enhance responsiveness across different screen sizes. Removed an unused hover transform effect in the menu navigation. These changes ensure better user experience on mobile and smaller devices.

--- elements/templates/pxl_search_form/layout-1.php
+++ elements/templates/pxl_search_form/layout-1.php
@@ -3,7 +3,7 @@
 	<form role="search" method="get" class="pxl-search-form <?php echo esc_attr($settings['pxl_animate']); ?>" action="<?php echo esc_url(home_url( '/' )); ?>" data-wow-delay="<?php echo esc_attr($settings['pxl_animate_delay']); ?>ms">
 		<div class="pxl-searchform-wrap">
 	        <input type="text" class="pxl-search-field" placeholder="<?php if(!empty($settings['email_placefolder'])) { echo esc_attr($settings['email_placefolder']); } else { esc_attr_e('Search here...', 'safebyte'); } ?>" name="s" />
-	    	<button type="submit" class="pxl-search-submit"><i class="flaticon-loupe"></i></button>
+	    	<button type="submit" class="pxl-search-submit"><i class="caseicon-search"></i></button>
 	    	<div class="pxl-searchform-divider"></div>
 	    </div>
 	</form>
