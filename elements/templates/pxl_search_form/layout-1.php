ce08349267086f82ce5c43291b2a3313d6f22590
hongducdev <contact.hongduc@gmail.com>
2025-04-15 11:31:11 +0700

    style: update search form styles and improve layout
    - Refactor search form styles for better alignment and responsiveness
    - Update font family to "Fira Sans" for consistency across elements
    - Enhance button and input styles for improved user experience
    - Modify search form template for better structure and functionality

--- elements/templates/pxl_search_form/layout-1.php
+++ elements/templates/pxl_search_form/layout-1.php
@@ -1,10 +1,10 @@
-<div class="pxl-search-form1 <?php echo esc_attr($settings['style']); ?>">
-	<h3 class="pxl-widget-title pxl-empty"><?php echo esc_html($settings['wg_title']); ?></h3>
-	<form role="search" method="get" class="pxl-search-form <?php echo esc_attr($settings['pxl_animate']); ?>" action="<?php echo esc_url(home_url( '/' )); ?>" data-wow-delay="<?php echo esc_attr($settings['pxl_animate_delay']); ?>ms">
-		<div class="pxl-searchform-wrap">
-	        <input type="text" class="pxl-search-field" placeholder="<?php if(!empty($settings['email_placefolder'])) { echo esc_attr($settings['email_placefolder']); } else { esc_attr_e('Search here...', 'safebyte'); } ?>" name="s" />
-	    	<button type="submit" class="pxl-search-submit"><i class="caseicon-search"></i></button>
-	    	<div class="pxl-searchform-divider"></div>
-	    </div>
-	</form>
+<?php
+	$placeholder = !empty($settings['placeholder']) ? $settings['placeholder'] : 'Search...';
+?>
+<div class="pxl-search-form pxl-search-form1 pxl-search-form-popup">
+	<div class="pxl-form-container">
+		<div class="pxl-form-inner">			
+			<?php get_search_form(); ?>
+		</div>
+	</div>
 </div>
